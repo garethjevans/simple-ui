66aeafd37994b1e6d4c5bf8539d5d402bf8320e8
Gareth Evans <gareth@bryncynfelin.co.uk>
2025-03-06 11:55:22 +0000

    chore: handle errors better on UI

--- src/main/java/com/vmware/tanzu/simpleui/ChatController.java
+++ src/main/java/com/vmware/tanzu/simpleui/ChatController.java
@@ -5,11 +5,11 @@ import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.ai.chat.client.ChatClient;
 import org.springframework.ai.chat.messages.AssistantMessage;
-import org.springframework.ai.chat.messages.Message;
 import org.springframework.ai.chat.messages.UserMessage;
-import org.springframework.ai.chat.prompt.ChatOptions;
 import org.springframework.ai.chat.prompt.Prompt;
-import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.ai.mcp.SyncMcpToolCallbackProvider;
+import org.springframework.ai.model.tool.ToolCallingChatOptions;
+import org.springframework.ai.retry.NonTransientAiException;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
@@ -23,32 +23,46 @@ public class ChatController {
 
     private static final Logger LOGGER = LoggerFactory.getLogger(ChatController.class);
 
-    private final ChatClient chatClient;
     private final ModelResolver modelResolver;
+    private final ChatClient client;
 
-    public ChatController(ChatClient.Builder chatClientBuilder, ModelResolver modelResolver) {
-        this.chatClient = chatClientBuilder.build();
+    public ChatController(ChatClient.Builder chatClientBuilder, ModelResolver modelResolver, SyncMcpToolCallbackProvider toolCallbackProvider) {
+        LOGGER.info("using ToolCallbackProvider {}", toolCallbackProvider);
+
         this.modelResolver = modelResolver;
+
+        var callbacks = toolCallbackProvider.getToolCallbacks();
+        for (var callback : callbacks) {
+            LOGGER.info("Got tool {}: {}", callback.getToolDefinition().name(), callback.getToolDefinition().description());
+        }
+
+        client = chatClientBuilder
+                .defaultTools(toolCallbackProvider)
+                .build();
     }
 
     @PostMapping(path={"/chat"})
     public List<Message> sendMessage(@RequestBody Request request) {
         LOGGER.info("Got request: {}", request);
+        Message response = null;
+        try {
+            String responseText = client.
+                    prompt(new Prompt(
+                                    convertMessages(request),
+                                    ToolCallingChatOptions.
+                                            builder().
+                                            model(request.model()).
+                                            build()
+                            )
+                    ).
+                    call().content();
 
-        String responseText = chatClient.
-                prompt(new Prompt(
-                        convertMessages(request),
-                        ChatOptions.
-                                builder().
-                                model(request.model()).
-                                build()
-                        )
-                ).
-                call().content();
+            response = new Message("assistant", responseText);
+        } catch (NonTransientAiException exp) {
+            response = new Message("error", exp.getMessage());
+        }
 
-        Message response = new Message("system", responseText);
         LOGGER.info("Got response: {}", response);
-
         return List.of(response);
     }
 
