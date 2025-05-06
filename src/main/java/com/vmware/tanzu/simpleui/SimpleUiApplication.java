5a78d35fb634d1e8ae96346e11a03cb3e966694c
Gareth Evans <gareth@bryncynfelin.co.uk>
2025-05-06 21:07:29 +0100

    chore: adding test configuration

--- src/main/java/com/vmware/tanzu/simpleui/SimpleUiApplication.java
+++ src/main/java/com/vmware/tanzu/simpleui/SimpleUiApplication.java
@@ -4,10 +4,10 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class SimpleuiApplication {
+public class SimpleUiApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(SimpleuiApplication.class, args);
+		SpringApplication.run(SimpleUiApplication.class, args);
 	}
 
 }
