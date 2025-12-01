7cd8e4046df8d2b6dabb1c453be93497de846373
Gareth Evans <gareth@bryncynfelin.co.uk>
2025-12-01 13:47:39 +0000

    chore: removing unused code

--- src/main/java/com/vmware/tanzu/simpleui/UiConfiguration.java
+++ src/main/java/com/vmware/tanzu/simpleui/UiConfiguration.java
@@ -12,28 +12,4 @@ public class UiConfiguration {
   public ChatModel chatModel(GenaiLocator locator) {
     return locator.getFirstAvailableChatModel();
   }
-
-  //    @Bean
-  //    public OpenAiApi openAiApi(OpenAiConnectionProperties commonProperties, OpenAiChatProperties
-  // chatProperties,
-  //                               ObjectProvider<RestClient.Builder> restClientBuilderProvider,
-  //                               ObjectProvider<WebClient.Builder> webClientBuilderProvider,
-  // ResponseErrorHandler responseErrorHandler) {
-  //
-  //        return OpenAiApi.builder()
-  //                .baseUrl(resolved.baseUrl())
-  //                .apiKey(new SimpleApiKey(resolved.apiKey()))
-  //                .completionsPath(chatProperties.getCompletionsPath())
-  //                .embeddingsPath(OpenAiEmbeddingProperties.DEFAULT_EMBEDDINGS_PATH)
-  //
-  // .restClientBuilder(restClientBuilderProvider.getIfAvailable(RestClient::builder))
-  //                .webClientBuilder(webClientBuilderProvider.getIfAvailable(WebClient::builder))
-  //                .responseErrorHandler(responseErrorHandler)
-  //                .build();
-  //    }
-
-  //    @Bean
-  //    public EmbeddingModel embeddingModel(GenaiLocator locator) {
-  //        return locator.getFirstAvailableEmbeddingModel();
-  //    }
 }
